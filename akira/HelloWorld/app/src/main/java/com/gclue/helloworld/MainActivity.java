c75d6d0c3c8725c30130c2a5ef795aa2fd249399
Akira-Sasaki <gclue.akira@gmail.com>
2015-03-29 09:11:23 +0900

    Add new JSON

--- akira/HelloWorld/app/src/main/java/com/gclue/helloworld/MainActivity.java
+++ akira/HelloWorld/app/src/main/java/com/gclue/helloworld/MainActivity.java
@@ -1,11 +1,21 @@
 package com.gclue.helloworld;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -21,17 +31,41 @@ public class MainActivity extends ActionBarActivity {
         mPaint.setStrokeWidth(10);
         mPaint.setARGB( 255, 255, 0, 0 );
 
-        // 線で描画.
-        canvas.drawLine( 0, 0, 500, 500, mPaint );
+                    // Contextを取得.
+                    final Context mContext =  this.getBaseContext();
 
-        // 描画するための線の色を設定.
-        mPaint = new Paint();
-        mPaint.setStyle( Paint.Style.FILL );
-        mPaint.setStrokeWidth(10);
-        mPaint.setARGB( 255, 255, 255, 0 );
+                    (new Thread(new Runnable() {
+                        @Override
+                        public void run() {
 
-        // 線で描画.
-        canvas.drawLine( 0, 0, 500, 500, mPaint2 );
+                            String response = "";
+                            DefaultHttpClient client = new DefaultHttpClient();
+                            HttpGet httpGet = new HttpGet("https://raw.githubusercontent.com/micknerd/Aizu/master/akira/data1.json");
+                            try {
+                                HttpResponse execute = client.execute(httpGet);
+                                InputStream content = execute.getEntity().getContent();
+
+                                BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                                String line = "";
+                                while ((line = buffer.readLine()) != null) {
+                                    response += line;
+                                }
+
+                                Log.i("LIFE", response);
+                            } catch (Exception e) {
+                                Log.i("LIFE", "error" + e);
+
+                            }
+
+                            // MYDATAという名前のSharedPreference.
+                            SharedPreferences settings = mContext.getSharedPreferences("MYDATA", mContext.MODE_PRIVATE);
+
+                            // SharePreferenceに保存.
+                            SharedPreferences.Editor editor = settings.edit();
+                            editor.putString("DATA", response);
+                            editor.commit();
+                        }
+                    })).start();
     }
 
     @Override
